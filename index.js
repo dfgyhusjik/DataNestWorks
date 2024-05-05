4ea6c2a6758a09525052b6a8b344f7bffa831806
dfgyhusjik <csjchobanihamza@hotmail.com>
2024-05-05 23:34:17 +0700

    hello

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-function longestPalindrome(s) {
-  const map = new Map();
-  let hasOdd = false;
-  let result = 0;
-  for (const char of s) {
-    map.set(char, (map.get(char) || 0) + 1);
-  }
-  for (const count of map.values()) {
-    if (count % 2 === 0) result += count;
-    else {
-      result += count - 1;
-      hasOdd = true;
-    }
-  }
-  return hasOdd ? result + 1 : result;
+function kthSmallest(matrix, k) {
+  const flatten = matrix.reduce((acc, row) => acc.concat(row), []);
+  flatten.sort((a, b) => a - b);
+  return flatten[k - 1];
 }
