38b2327604fadca953975a5ef10aa65f9f15e1dc
dfgyhusjik <csjchobanihamza@hotmail.com>
2024-05-09 07:27:06 +0700

    First commit

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-function rob(nums) {
-  let prevMax = 0;
-  let currMax = 0;
-  for (const num of nums) {
-    const temp = currMax;
-    currMax = Math.max(prevMax + num, currMax);
-    prevMax = temp;
-  }
-  return currMax;
+function minDepth(root) {
+  if (!root) return 0;
+  if (!root.left && !root.right) return 1;
+  let min = Infinity;
+  if (root.left) min = Math.min(min, minDepth(root.left));
+  if (root.right) min = Math.min(min, minDepth(root.right));
+  return min + 1;
 }
