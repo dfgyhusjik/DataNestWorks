96bead3ae744d23fa11fe9e4ada928cbb43d3d50
dfgyhusjik <csjchobanihamza@hotmail.com>
2024-05-14 06:00:49 +0700

    update commit

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const selectionSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-    if (minIndex !== i) {
-      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-    }
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
   }
-  return arr;
-};
+  return false;
+}
